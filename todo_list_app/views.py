10dc9299ee82ba66c997552b0e35fd5f7d1e37b8
issam <issame.hassane@hotmail.com>
2025-09-11 11:57:56 +0200

    app

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Bienvenue sur ma To-Do List </h1><p>Ceci est une page d'exemple.</p>")
