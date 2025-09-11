5313e9ace1f65cd75b88eb894fa9f1328ca6f2e9
Shiraishi <ryan.mazri@yahoo.fr>
2025-09-11 16:29:15 +0200

    added gitla cicd

--- todo_list_app/tests.py
+++ todo_list_app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    def test_task_creation(self):
+        task = Task.objects.create(title="Test Task", priority="high")
+        self.assertEqual(task.title, "Test Task")
+        self.assertEqual(task.priority, "high")
+        self.assertFalse(task.is_completed)
+
+class TaskViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def test_task_list_view(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_create_task(self):
+        response = self.client.post(reverse('task_list'), {
+            'title': 'New Task',
+            'priority': 'medium'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Task.objects.filter(title='New Task').exists())
